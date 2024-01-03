7c1e1b444a4a2993970b9d686f8703fd9b643840
Kuba <jakub@slys.dev>
2024-01-03 12:04:56 +0000

    Added launch.json file

--- sandbox/main.py
+++ sandbox/main.py
@@ -15,10 +15,29 @@ def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
 
+def singleton(cls):
+    instances = {}
+
+    def wrapper(*args, **kwargs):
+        if cls not in instances:
+            instances[cls] = cls(*args, **kwargs)
+        return instances[cls]
+    return wrapper
+
+
+@singleton
+class MySingleton():
+    def foo(self):
+        pass
+
+
 def main():
     print('Hello world!!!')
+    s1 = MySingleton()
+    s2 = MySingleton()
+    print(id(s1))
+    print(id(s2))
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True, log_level="debug",
-                workers=1, limit_concurrency=1, limit_max_requests=1)
+    main()
